5b63d5eb065f78a01e06e7696a5eaa94a2ad2979
Pravin751 <72076237+Pravin751@users.noreply.github.com>
2021-06-15 10:59:44 +0530

    COMMIT

--- csv main1/app/views.py
+++ csv main1/app/views.py
@@ -2,24 +2,20 @@ import csv, io
 from django.shortcuts import render
 from django.contrib import messages
 from . models import *
-# Create your views here.
-# one parameter named request
+
 def profile_upload(request):
-    # declaring template
     
-    
-# prompt is a context variable that can have different values      depending on their context
     prompt={'order':'Order of the CSV should be name,email,address,phone,profile'}
     
-    # GET request returns the value of the data with the specified key.
+    
     if request.method == "GET":
         return render(request, 'profile_upload.html', prompt)
     csv_file = request.FILES['file']
-    # let's check if it is a csv file
+   
     if not csv_file.name.endswith('.csv'):
         messages.error(request, 'THIS IS NOT A CSV FILE')
     data_set = csv_file.read().decode('UTF-8')
-    # setup a stream which is when we loop through each line we are able to handle a data in a stream
+    
     io_string = io.StringIO(data_set)
     next(io_string)
     for column in csv.reader(io_string, delimiter=',', quotechar="|"):
